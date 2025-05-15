6a9f274ba6b1a6ec4ec8db6ce9cc66ff190ab07b
adrus <permasadrian@gmail.com>
2025-05-15 18:15:58 +0200

    Codigo ensamblador de main.c

---
+++ tutorial/raceConditions/main.s
@@ -0,0 +1,171 @@
+	.file	"main.c"
+	.text
+	.globl	mails
+	.bss
+	.align 4
+	.type	mails, @object
+	.size	mails, 4
+mails:
+	.zero	4
+	.text
+	.globl	rutina1
+	.type	rutina1, @function
+rutina1:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	mails(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, mails(%rip)
+	addl	$1, -4(%rbp)
+.L2:
+	cmpl	$999999, -4(%rbp)
+	jle	.L3
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	rutina1, .-rutina1
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Error al crear un nuevo thread\n"
+	.align 8
+.LC1:
+	.string	"Error al esperar a que termine de ejecutarse el thread / hilo\n"
+.LC2:
+	.string	"Numero de mails enviados: %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB7:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movl	%edi, -36(%rbp)
+	movq	%rsi, -48(%rbp)
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	-24(%rbp), %rax
+	movl	$0, %ecx
+	leaq	rutina1(%rip), %rdx
+	movl	$0, %esi
+	movq	%rax, %rdi
+	call	pthread_create@PLT
+	testl	%eax, %eax
+	je	.L5
+	movq	stderr(%rip), %rax
+	movq	%rax, %rcx
+	movl	$31, %edx
+	movl	$1, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	call	fwrite@PLT
+	movl	$-1, %eax
+	jmp	.L10
+.L5:
+	leaq	-16(%rbp), %rax
+	movl	$0, %ecx
+	leaq	rutina1(%rip), %rdx
+	movl	$0, %esi
+	movq	%rax, %rdi
+	call	pthread_create@PLT
+	testl	%eax, %eax
+	je	.L7
+	movq	stderr(%rip), %rax
+	movq	%rax, %rcx
+	movl	$31, %edx
+	movl	$1, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	call	fwrite@PLT
+	movl	$-1, %eax
+	jmp	.L10
+.L7:
+	movq	-24(%rbp), %rax
+	movl	$0, %esi
+	movq	%rax, %rdi
+	call	pthread_join@PLT
+	testl	%eax, %eax
+	je	.L8
+	movq	stderr(%rip), %rax
+	movq	%rax, %rcx
+	movl	$62, %edx
+	movl	$1, %esi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	call	fwrite@PLT
+	movl	$-1, %eax
+	jmp	.L10
+.L8:
+	movq	-16(%rbp), %rax
+	movl	$0, %esi
+	movq	%rax, %rdi
+	call	pthread_join@PLT
+	testl	%eax, %eax
+	je	.L9
+	movq	stderr(%rip), %rax
+	movq	%rax, %rcx
+	movl	$62, %edx
+	movl	$1, %esi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	call	fwrite@PLT
+	movl	$-1, %eax
+	jmp	.L10
+.L9:
+	movl	mails(%rip), %edx
+	movq	stdout(%rip), %rax
+	leaq	.LC2(%rip), %rcx
+	movq	%rcx, %rsi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	fprintf@PLT
+	movl	$0, %eax
+.L10:
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L11
+	call	__stack_chk_fail@PLT
+.L11:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE7:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
